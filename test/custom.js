8ee2afae363bc0a46b18e2215d090ef3818713b1
andreev75 <139889159+andreev75@users.noreply.github.com>
2023-07-18 17:52:55 +0300

    first commit
    upload file

---
+++ test/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $('.sli_block').slick({
+        infinite: true,
+        autoplay: true,
+        dots: true,
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
